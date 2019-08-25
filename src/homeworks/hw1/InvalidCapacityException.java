b8fd842c444b8a12e843d32049688b6eab5f9368
Jason Filippou <jason.filippou@gmail.com>
2019-08-25 08:39:00 -0400

    Generated docs for MyStackClient.java.

--- src/homeworks/hw1/InvalidCapacityException.java
+++ src/homeworks/hw1/InvalidCapacityException.java
@@ -1,7 +1,7 @@
 package homeworks.hw1;
 
 /**
- *  {@link InvalidCapacityException} instances are thrown from {@link MyStack#MyStack(int)} when the user
+ *  {@link InvalidCapacityException} instances are thrown from {@link MyStack#MyStack(int, StackSize)} when the user
  *  provides a non-positive capacity for the {@link MyStack} instance.
  *
  * @see MyStack
