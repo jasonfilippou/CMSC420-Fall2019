25c25b9ca7b0e1cfa7d3991d75e1e0e994262e7a
Jason Filippou <jason.filippou@gmail.com>
2019-11-07 14:16:13 -0500

    Project ready for release.

---
+++ src/projects/bpt/BinaryPatriciaTrie.java
@@ -0,0 +1,145 @@
+package projects.bpt;
+
+import projects.UnimplementedMethodException;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+/**
+ * <p>{@code BinaryPatriciaTrie} is a Patricia Trie over the binary alphabet &#123;	 0, 1 &#125;. By restricting themselves
+ * to this small but terrifically useful alphabet, Binary Patricia Tries combine all the positive
+ * aspects of Patricia Tries while shedding the storage cost typically associated with tries that
+ * deal with huge alphabets.</p>
+ *
+ * @author YOUR NAME HERE!
+ */
+public class BinaryPatriciaTrie {
+
+    /* We are giving you this class as an example of what your inner node might look like.
+     * If you would prefer to use a size-2 array or hold other things in your nodes, please feel free
+     * to do so. We can *guarantee* that a *correct* implementation exists with *exactly* this data
+     * stored in the nodes.
+     */
+    private static class TrieNode {
+        private TrieNode left, right;
+        private String str;
+        private boolean isKey;
+
+        // Default constructor for your inner nodes.
+        TrieNode() {
+            this("", false);
+        }
+
+        // Non-default constructor.
+        TrieNode(String str, boolean isKey) {
+            left = right = null;
+            this.str = str;
+            this.isKey = isKey;
+        }
+    }
+
+    /**
+     * Simple constructor that will initialize the internals of {@code this}.
+     */
+    public BinaryPatriciaTrie() {
+        throw new UnimplementedMethodException(); // ERASE THIS LINE AFTER YOU IMPLEMENT THE METHOD!
+    }
+
+    /**
+     * Searches the trie for a given key.
+     *
+     * @param key The input {@link String} key.
+     * @return true if and only if key is in the trie, false otherwise.
+     */
+    public boolean search(String key) {
+        throw new UnimplementedMethodException(); // ERASE THIS LINE AFTER YOU IMPLEMENT THE METHOD!
+    }
+
+    /**
+     * Inserts key into the trie.
+     *
+     * @param key The input {@link String}  key.
+     * @return true if and only if the key was not already in the trie, false otherwise.
+     */
+    public boolean insert(String key) {
+        throw new UnimplementedMethodException(); // ERASE THIS LINE AFTER YOU IMPLEMENT THE METHOD!
+    }
+
+
+    /**
+     * Deletes key from the trie.
+     *
+     * @param key The {@link String}  key to be deleted.
+     * @return True if and only if key was contained by the trie before we attempted deletion, false otherwise.
+     */
+    public boolean delete(String key) {
+        throw new UnimplementedMethodException(); // ERASE THIS LINE AFTER YOU IMPLEMENT THE METHOD!
+    }
+
+    /**
+     * Queries the trie for emptiness.
+     *
+     * @return true if and only if {@link #getSize()} == 0, false otherwise.
+     */
+    public boolean isEmpty() {
+        throw new UnimplementedMethodException(); // ERASE THIS LINE AFTER YOU IMPLEMENT THE METHOD!
+    }
+
+    /**
+     * Returns the number of keys in the tree.
+     *
+     * @return The number of keys in the tree.
+     */
+    public int getSize() {
+        throw new UnimplementedMethodException(); // ERASE THIS LINE AFTER YOU IMPLEMENT THE METHOD!
+    }
+
+    /**
+     * <p>Performs an <i>inorder (symmetric) traversal</i> of the Binary Patricia Trie. Remember from lecture that inorder
+     * traversal in tries is NOT sorted traversal, unless all the stored keys have the same length. This
+     * is of course not required by your implementation, so you should make sure that in your tests you
+     * are not expecting this method to return keys in lexicographic order. We put this method in the
+     * interface because it helps us test your submission thoroughly and it helps you debug your code! </p>
+     *
+     * <p>We <b>neither require nor test </b> whether the {@link Iterator} returned by this method is fail-safe or fail-fast.
+     * This means that you  do <b>not</b> need to test for thrown {@link java.util.ConcurrentModificationException}s and we do
+     * <b>not</b> test your code for the possible occurrence of concurrent modifications.</p>
+     *
+     * <p>We also assume that the {@link Iterator} is <em>immutable</em>, i,e we do <b>not</b> test for the behavior
+     * of {@link Iterator#remove()}. You can handle it any way you want for your own application, yet <b>we</b> will
+     * <b>not</b> test for it.</p>
+     *
+     * @return An {@link Iterator} over the {@link String} keys stored in the trie, exposing the elements in <i>symmetric
+     * order</i>.
+     */
+    public Iterator<String> inorderTraversal() {
+        throw new UnimplementedMethodException(); // ERASE THIS LINE AFTER YOU IMPLEMENT THE METHOD!
+    }
+
+    /**
+     * Finds the longest {@link String} stored in the Binary Patricia Trie.
+     * @return <p>The longest {@link String} stored in this. If the trie is empty, the empty string &quot;&quot; should be
+     * returned. Careful: the empty string &quot;&quot;is <b>not</b> the same string as &quot; &quot;; the latter is a string
+     * consisting of a single <b>space character</b>! It is also <b>not the same as the</b> null <b>reference</b>!</p>
+     *
+     * <p>Ties should be broken in terms of <b>value</b> of the bit string. For example, if our trie contained
+     * only the binary strings 01 and 11, <b>11</b> would be the longest string. If our trie contained
+     * only 001 and 010, <b>010</b> would be the longest string.</p>
+     */
+    public String getLongest() {
+        throw new UnimplementedMethodException(); // ERASE THIS LINE AFTER YOU IMPLEMENT THE METHOD!
+    }
+
+    /**
+     * Makes sure that your trie doesn't have splitter nodes with a single child. In a Patricia trie, those nodes should
+     * be pruned. Be careful with the implementation of this method, since our tests call it to make sure your deletions work
+     * correctly! That is to say, if your deletions work well, but you have made an error in this (far easier) method,
+     * you will <b>still</b> not be passing our tests!
+     *
+     * @return true iff all nodes in the trie either denote stored strings or split into two subtrees, false otherwise.
+     */
+    public boolean isJunkFree(){
+        throw new UnimplementedMethodException(); // ERASE THIS LINE AFTER YOU IMPLEMENT THE METHOD!return isEmpty() || (isJunkFree(root.left) && isJunkFree(root.right));
+    }
+}
